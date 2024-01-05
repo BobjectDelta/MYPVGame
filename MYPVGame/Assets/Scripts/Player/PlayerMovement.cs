52c4953e7bd83372e90ecfbc01132c10700d2bfc
BobjectDelta <Nickpav1337@gmail.com>
2024-01-05 15:59:15 +0100

    Joystick and touch input support
    Added controls for mobile devices
    Integrated them with GameManager and PlayerMovement systems
    Some minor tweaks

--- MYPVGame/Assets/Scripts/Player/PlayerMovement.cs
+++ MYPVGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed = 4f;
-
     [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private Joystick _movementJoystick;
 
     Vector2 movementVector;
+
+    private void Start()
+    {
+        //_movementJoystick = GameObject.Find("MovementJoystick").GetComponent<Joystick>();
+    }
     void Update()
     {
-        movementVector.x = Input.GetAxisRaw("Horizontal");
-        movementVector.y = Input.GetAxisRaw("Vertical");
-
+        //movementVector.x = Input.GetAxisRaw("Horizontal");
+        //movementVector.y = Input.GetAxisRaw("Vertical");
+        movementVector.x = _movementJoystick.Horizontal;
+        movementVector.y = _movementJoystick.Vertical;
     }
 
     private void FixedUpdate()
@@ -26,6 +32,11 @@ public class PlayerMovement : MonoBehaviour
         _movementSpeed += speedPoints;
         if (_movementSpeed < 1)
             _movementSpeed = 1;
+    }
+
+    public void SetMovementJoystick(GameObject joystick)
+    {
+        _movementJoystick = joystick.GetComponent<Joystick>();
     }
 
     public float GetMovementSpeed()
--- MYPVGame/Assets/Scripts/Player/Shooting/ShootingInput.cs
+++ MYPVGame/Assets/Scripts/Player/Shooting/ShootingInput.cs
@@ -6,30 +6,45 @@ public class ShootingInput : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody2D;
     [SerializeField] private Camera _camera;
-
+    [SerializeField] private Joystick _shootingJoystick;
     [SerializeField] private Shooting _shootingType;
 
     Vector2 mousePosition;
+    Vector2 lastLookDirection;
+    Vector2 lookDirection;
+    float angle;
+
 
     private void Start()
     {
         _camera = Camera.main;
         _camera.GetComponent<FollowingCamera>().SetTarget(gameObject);
+        //_shootingJoystick = GameObject.Find("ShootingJoystick").GetComponent<Joystick>();
     }
 
     void Update()
     {
-        mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        //mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButton(0))
+        //if (Input.GetMouseButton(0))
+        if (Mathf.Abs(_shootingJoystick.Horizontal) >= 0.1f || Mathf.Abs(_shootingJoystick.Vertical) >= 0.1f)
             _shootingType.Shoot();
     }
 
     private void FixedUpdate()
     {
-        Vector2 lookDirection = mousePosition - _rigidbody2D.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        if (_shootingJoystick.Direction != Vector2.zero)
+        {
+            lookDirection = _shootingJoystick.Direction;//mousePosition - _rigidbody2D.position;
+            lastLookDirection = lookDirection;
+        }
+        angle = Mathf.Atan2(lastLookDirection.y, lastLookDirection.x) * Mathf.Rad2Deg - 90f;
         _rigidbody2D.rotation = angle;
+    }
+
+    public void SetShootingJoystick(GameObject joystick)
+    {
+        _shootingJoystick = joystick.GetComponent<Joystick>();
     }
 
     public Shooting GetShootingType() 
