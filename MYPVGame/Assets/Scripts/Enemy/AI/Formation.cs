21538896507013e65fbefbc0a42655aad6dd3826
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-12-02 21:55:34 +0200

    Added formation.

---
+++ MYPVGame/Assets/Scripts/Enemy/AI/Formation.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Formation : MonoBehaviour
+{
+    private static int _idCounter = -1;
+    [SerializeField] private int id = ++_idCounter;
+    private List<Enemy> _enemies { get; set; }
+
+    private void Awake()
+    {
+        _enemies = new List<Enemy> { GetComponent<Enemy>() };
+    }
+
+    public void Merge(Formation formationToMerge)
+    {
+        foreach (Enemy enemy in formationToMerge._enemies)
+        {
+            enemy._formation = this;
+        }
+        _enemies.AddRange(formationToMerge._enemies);
+    }
+}
